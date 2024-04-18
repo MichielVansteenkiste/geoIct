818b8392964d1d391fb06420bfdbfb5b30d9e1f2
MichielVansteenkiste <126386204+MichielVansteenkiste@users.noreply.github.com>
2024-04-18 15:34:46 +0200

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,9 @@
+$("#suggestionInput").autocomplete({
+    preProcess: (suggestionsResponse) => {
+        return suggestionsResponse.SuggestionResult;
+    },
+    filterMinChars: 3,
+    onPick(el, item) {
+        alert("User picked " + item.textContent);
+    },
+});
